83c1853d63c566552b908ac22fdde2c26b4d9c36
anabneri <ananeri32@hotmail.com>
2022-11-03 18:35:12 -0300

    inserting the validation fields

---
+++ mocks-aula01/src/constants.js
@@ -0,0 +1,8 @@
+const constants = {
+    error: {
+        FILE_LENGTH_ERROR_MESSAGE: 'The content length is invalid!',
+        FILE_FIELDS_ERROR_MESSAGE: 'The provided properties are invalid!'
+    }
+}
+
+module.exports = constants
---
+++ mocks-aula01/src/file.js
@@ -0,0 +1,62 @@
+const { readFile } = require('fs/promises')
+// pattern to normalize the path
+const { join} = require('path')
+const { error } = require('./constants')
+
+const DEFAULT_OPTION = {
+    maxLines: 3,
+    fields: ["id","name","profession","age"]
+    
+}
+
+class File {
+    static async csvToJson(filePath) {
+        const content = await File.getfileContent(filePath)
+        const validation = File.isValid(content)
+        if(!validation.valid) throw new Error(validation.error)
+
+        return content
+    }
+
+    // read the content according to the path
+    static async getfileContent(filePath) {
+        const fileName = join(__dirname, filePath)
+
+        //encoding
+        return (await readFile(fileName)).toString("utf8")
+    }
+
+    static isValid(csvStirng, options  = DEFAULT_OPTION) {
+        const [header, ...fileWithoutHeader] = csvStirng.split('\n')
+        const isHeaderValid = header === options.fields.join(',')
+        if(!isHeaderValid) {
+            return {
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        const isContentLengthAccepted = (
+            fileWithoutHeader.length > 0 &&
+            fileWithoutHeader.length <= options.maxLines
+        )
+        if(!isContentLengthAccepted) {
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false,
+            }
+        }
+
+        return { valid: true }
+
+    }
+}
+
+(async () => {
+    const result  = await File.csvToJson('./../mocks/threeItems-valid.csv')
+    // const result  = await File.csvToJson('./../mocks/fourItems-invalid.csv')
+    // const result  = await File.csvToJson('./../mocks/invalid-header.csv')
+
+    console.log("result", result)
+    }
+)();
