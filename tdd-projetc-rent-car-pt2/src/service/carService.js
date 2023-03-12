72c9a2232078f0ed92dcdb72d94b369c18eeaac2
Ana Neri <ananeri32@hotmail.com>
2023-03-12 14:56:03 -0300

    test(): creating unit tests
    Signed-off-by: Ana Neri <ananeri32@hotmail.com>

--- tdd-projetc-rent-car-pt2/src/service/carService.js
+++ tdd-projetc-rent-car-pt2/src/service/carService.js
@@ -5,8 +5,27 @@ class CarService {
         this.carRepository = new BaseRepository({ file: cars })
     }
 
+    getRandomPositionFromArray(list) {
+        const listLength = list.length
+        return Math.floor(
+            Math.random() * (listLength)
+        )
+    }
+
+    chooseRandomCar(carCategory) {
+        const randomCarIndex = this.getRandomPositionFromArray(carCategory.carIds)
+        const carId = carCategory.carIds[randomCarIndex]
+
+        return carId
+
+    }
+
+
     async getAvailableCar(carCategory) {
-        return null
+        const carId = this.chooseRandomCar(carCategory)
+        const car = await this.carRepository.find(carId)
+
+        return car
     }
 
 }
--- tdd-projetc-rent-car-pt2/test/unitTests/carsService.test.js
+++ tdd-projetc-rent-car-pt2/test/unitTests/carsService.test.js
@@ -1,36 +1,81 @@
-const { describe, it} = require('mocha') 
+const { describe, it, beforeEach, afterEach} = require('mocha') 
 const CarService = require('./../../src/service/carService')
 
 const { join } = require('path')
-const { expected } = require('chai')
+const { expect } = require('chai')
+const sinon = require('sinon')
 
 const carsDatabase = join(__dirname, './../../database', "cars.json")
 
 const mocks = {
     validCarCategory: require('./../mocks/valid-carCategory.json'),
     validCar: require('./../mocks/valid-car.json'),
-    validCustomer: require('./../mocks/valid')
+    validCustomer: require('./../mocks/valid-customer.json')
 }
 
 
 describe('CarService Suite Tests', () => {
     let carService = {}
+    let sandbox = {}
     before(() => {
         carService = new CarService({
             cars: carsDatabase
         })
     })
 
+    beforeEach(() => {
+        sandbox = sinon.createSandbox()
+    })
+
+    afterEach(() => {
+        sandbox.restore()
+    })
+
+    it('should retrieve a ramdom position from an array', () => {
+        const data = [0, 1, 2, 3, 4]
+        const result = carService.getRandomPositionFromArray(data)
+
+        expect(result).to.be.lte(data.length).and.be.gte(0)
+    })
+
+    it('should choose the first id from carId', () => {
+        const carCategory = mocks.validCarCategory
+        const carIdIndex = 0
+
+        sandbox.stub(
+            carService,
+            carService.getRandomPositionFromArray.name
+        ).returns(carIdIndex)
+
+        const result = carService.chooseRandomCar(carCategory)
+        const expected = carCategory.carIds[carIdIndex]
+
+        expect(carService.getRandomPositionFromArray.calledOnce).to.be.ok
+        expect(result).to.be.equal(expected)
+    })
 
     it('given a carCategory it should return an available car', async () => {
         const car = mocks.validCar
         const carCategory = Object.create(mocks.validCarCategory)
-        carCategory.ids = [car.id]
+        carCategory.carIds = [car.id]
+
+        sandbox.stub(
+            carService.carRepository,
+            carService.carRepository.find.name,
+        ).resolves(car)
+
+
+        sandbox.spy(
+            carService,
+            carService.chooseRandomCar.name,
+        )
 
         const result = await carService.getAvailableCar(carCategory)
         const expected = car
 
-        expected(result).to.be.deep.equal(expected)
+        expect(carService.chooseRandomCar.calledOnce).to.be.ok
+        expect(result).to.be.deep.equal(expected)
 
     })
+
 })
