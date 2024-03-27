9f209dc3b069495681f214e92b503a9c43ffc4fc
ananeridev <ananeri32@hotmail.com>
2024-03-27 13:42:27 -0300

    test: regex
    Signed-off-by: ananeridev <ananeri32@hotmail.com>

--- mod-5-regex/processing-pdf/app/test/textProcessorFluentAPI.test.js
+++ mod-5-regex/processing-pdf/app/test/textProcessorFluentAPI.test.js
@@ -27,4 +27,20 @@ describe('TextProcessorFluentAPI', () => {
     expect(result).to.be.deep.equal(expected)
 
     })
+    it('#divideTextInColumns', () => { 
+        const result = new TextProcessorFluentAPI(mock)
+        .extractPeopleData()
+        .divideTextInColumns()
+        .build()
+        const expected = [
+        [
+            "Xuxa da Silva, brasileira, casada, CPF 235.743.420-12, residente e ",
+            "domiciliada a Rua dos bobos, zero, bairro Alphaville, São Paulo. "
+        ],
+        [
+            "Arya Robbin, belga, casado, CPF 884.112.200-52, residente e ",
+            "domiciliada a Av. paulista, 1400, bairro Consolação, São Paulo."
+        ]
+    ]
+    })
 })
